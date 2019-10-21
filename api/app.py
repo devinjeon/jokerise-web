761ac664181ca849a3cb5ebe4a450d9e214ad5ae
devinjeon <gywns2702@gmail.com>
2019-10-21 23:52:20 +0900

    Just reformat

--- api/app.py
+++ api/app.py
@@ -6,8 +6,8 @@ import numpy as np
 import os
 import xxhash
 
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
+# If `entrypoint` is not defined in app.yaml,
+# App Engine will look for an app called `app` in `main.py`.
 app = Flask(__name__)
 predictor = None
 
