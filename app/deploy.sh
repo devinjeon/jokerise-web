9be5ff489f2f3906cf3fe09a44731b5aaa723d8c
devinjeon <gywns2702@gmail.com>
2019-10-31 04:36:50 +0900

    Make SCRIPT_DIR to make sure that we can run anywhere the script

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+cd "$SCRIPT_DIR" || exit 1
+
 npm run lint
 npm run build
 
-gsutil rsync -R ./dist/ gs://"$(cat ./custom_domain)"
+# shellcheck disable=SC2086
+gsutil rsync -R "$SCRIPT_DIR/dist/" gs://"$(cat $SCRIPT_DIR/custom_domain)"
