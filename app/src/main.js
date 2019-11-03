5444d20cc4b1d6fd3d4fd11ada35b7fc7341556a
devinjeon <gywns2702@gmail.com>
2019-11-04 01:07:39 +0900

    Use Sentry for application error tracking

--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
 import VueAnalytics from 'vue-analytics'
 import './plugins/axios'
 import './plugins/bootstrap-vue'
 
 import App from './App.vue'
+
+Sentry.init({
+  dsn: 'https://25b2563098514cf7af24e9fc663513a8@sentry.io/1806124',
+  integrations: [new Integrations.Vue({ Vue, attachProps: true })],
+  environment: process.env.VUE_APP_ENV
+})
 
 Vue.config.productionTip = false
 Vue.use(VueAnalytics, {
